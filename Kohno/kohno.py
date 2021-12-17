795b6973590c4aa17cce92fb543caf4248fffea8
KkohnoNikolay <kohnonikolay@gmail.com>
2021-12-17 21:13:50 +0300

    лишний мув

--- Kohno/kohno.py
+++ Kohno/kohno.py
@@ -1,9 +1,5 @@
-
-
-exit()
-
 print("fjekfjkgj")
 mkdir_file = open("FirstWork", "w+")
-mkdir_file.write("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")
+mkdir_file.write("2 string")
 mkdir_file.close()
 print("Greate")
