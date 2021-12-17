f00155d54e8fa7d1a73f60a5c978b41e020fc699
KkohnoNikolay <kohnonikolay@gmail.com>
2021-12-17 20:10:42 +0300

    Добавление токена

--- Kohno/kohno.py
+++ Kohno/kohno.py
@@ -1,5 +1,5 @@
 print("fjekfjkgj")
 mkdir_file = open("FirstWork", "w+")
-mkdir_file.write("Создание файла через PyCharm.")
+mkdir_file.write("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")
 mkdir_file.close()
 print("Greate")
