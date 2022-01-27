f66956b00514349017e6b5114c0937a077a13db1
Николай Кохно <90795569+KohnoNikolay@users.noreply.github.com>
2022-01-27 18:58:25 +0300

    Create HW_1_1.py
    Done HW_1_1

---
+++ Kohno/HW_1_1.py
@@ -0,0 +1,12 @@
+'''
+1. Даны 2 действительных числа a и b. Получить их сумму, разность и
+произведение.
+'''
+
+a = 10
+b = 2
+summa = a + b
+raznost = a - b
+proizvedenie = a * b
+
+print(summa, raznost, proizvedenie)
